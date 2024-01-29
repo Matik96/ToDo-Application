295b868237edc21224e63517372acc94d02d8e1e
a437226 <mateusz.kowalski@consultant.volvo.com>
2024-01-29 16:10:02 +0100

    refactor code

--- src/main/java/io/github/mat3e/model/SqlTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.mat3e.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
-}
